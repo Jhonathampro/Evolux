3084a422e4f7c9ab6bd655026eab408b9414e05f
Jhonatham Jesus <jhonathampro@gmail.com>
2025-01-27 14:13:43 -0300

    Colocando um formulário

--- js/forms.js
+++ js/forms.js
@@ -1,121 +1,262 @@
-let Tempo = 0;
-let primeiro = 0;
-let segundo = 0;
-let terceiro = 0;
-let quarto = 0;
-let porcentagem = 0;
-// criei três variáveis para ajudar para quem ler consiguer entender, elas são
-// primeiro, segundo, terceiro e quarto porém dava para fazer só com uma e chamar ela de valor
 
-if ('#primeiraSemFalemais'){
-    // Primeiro faço o cálculo do Sem falemais
-     Tempo = 20;
-     Tempo *= 1.90; 
-     primeiro = Tempo;
-     console.log(Tempo) 
-     // O if faz a verificação se o tempo é positivo, porém eu pensei na ideia se 
-     // a relação de origem e destino existe na tabela 
-     if(Tempo > 0){
-     document.querySelector('#primeiraSemFalemais').textContent = formatarMoeda(primeiro);
-    //  Fazendo com o plano faleMais
-    Tempo = 20 - 30;
-    // faço a verificação se o tempo é maior que zero para determinar 
-    // se o plano cobre a ligação
-    if(Tempo > 0 ){
-        Tempo *= 1.90;
-        // O tempo faz um papel de valor e minutos, fiz isso para aproveitar a váriavel
-        porcentagem = Tempo * 0.10;
-        primeiro =  Tempo + porcentagem  
-        document.querySelector('#primeiraComFalemais').textContent = formatarMoeda(primeiro); 
-    }else{
-        primeiro = 0;
-        document.querySelector('#primeiraComFalemais').textContent = formatarMoeda(primeiro); 
-    }
-    // Esse else funciona caso não exista origem e destino 
-    // entre os DDD
-} else{
-    document.querySelector('#primeiraSemFalemais').textContent = "-";
-    document.querySelector('#primeiraComFalemais').textContent = "-"; 
-}
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
     
-// os outros codigos tem a mesma função então não estão comentados como o primeiro
+        let origem = document.querySelector('#origem').value;
+        let destino = document.querySelector('#destino').value;
+        let plano = document.querySelector('#plano').value;
+        let minuto = document.querySelector('#minuto').value;
+        const minutoFixo = minuto;
+        let comFale = 0;
+        let semFale = 0;
+        let porcentagem = 0;
+        let tempo = 0;
 
-
-}  if ('#segundoSemFalemais'){
-    Tempo = 80;
-    Tempo *= 1.70; 
-    segundo = Tempo;
-    console.log(Tempo)
-    if(Tempo > 0){
-    document.querySelector('#segundoSemFalemais').textContent = formatarMoeda(segundo);
-     //  Fazendo com o plano faleMais
-    Tempo = 80 - 60;
-    if(Tempo > 0 ){
-         Tempo *= 1.70;
-         porcentagem = Tempo * 0.10;
-         segundo =  Tempo + porcentagem    
-        document.querySelector('#segundoComFalemais').textContent = formatarMoeda(segundo);
-    }else{
-        segundo = 0;
-        document.querySelector('#segundoComFalemais').textContent = formatarMoeda(segundo);
-    }
-}  else {
-    document.querySelector('#segundoSemFalemais').textContent = "-";
-    document.querySelector('#segundoComFalemais').textContent = "-";
-}
-      
-
-}   if ('#terceiraSemFalemais'){ 
-    Tempo = 200;
-    Tempo *= 1.90; 
-    terceiro = Tempo;
-    console.log(Tempo)  
-    if(Tempo > 0){
-    document.querySelector('#terceiroSemFalemais').textContent = formatarMoeda(terceiro); 
-       //  Fazendo com o plano faleMais
-     Tempo = 200 - 120;
-     if(Tempo > 0 ){
-        Tempo *= 1.90;
-        porcentagem = Tempo * 0.10;
-        terceiro =  Tempo + porcentagem 
-        document.querySelector('#terceiroComFalemais').textContent = formatarMoeda(terceiro); 
-     }else{
-         terceiro = 0;
-         document.querySelector('#terceiroComFalemais').textContent = formatarMoeda(terceiro); 
-     }
-    } else{
-        document.querySelector('#terceiroSemFalemais').textContent = "-";
-        document.querySelector('#terceiroComFalemais').textContent = "-"; 
-    }
+        if(origem == '011' && destino == '016'){
+            // sem o palno falemais
+             semFale = minutoFixo * 1.90;
+             // aqui pego o plano dogitado pelo usuário
+                if(plano == 'FaleMais30'){
+                    tempo = 30;
+                } else if(plano == 'FaleMais60'){
+                    tempo = 60;
+                } else if(plano == 'FaleMais120'){
+                    tempo = 120;
+                }
+                // nessa parte faço o minuto digitado pelo usuário menos o plano selecionado 
+               minuto -= tempo;
+               // aqui vejo se o minuto é positivo 
+                if(minuto > 0){
+                    // faço o novo minuto
+                    minuto *= 1.90;
+                    // faço a nova porcentagem
+                    porcentagem = minuto * 0.10;
+                    // faço o novo minoto mais a porcentagem
+                    comFale =  minuto + porcentagem; 
+                } else  { 
+                // esse caso o plano cubra o tempo da ligação
+                comFale = 0;
+                
+             }
+             const tr = document.createElement('tr');
+             tr.innerHTML = `
+             <td>${origem}</td>
+             <td>${destino}</td>
+             <td>${plano}</td>
+             <td>${minutoFixo}</td>
+             <td>${formatarMoeda(comFale)}</td>
+             <td>${formatarMoeda(semFale)}</td>
     
-        
-}
-
-if ('#quartoSemFalemais'){ 
-    Tempo = 100;
-    Tempo *= 0; 
-    quarto = Tempo;
-    console.log(Tempo)  
-    if(Tempo > 0){
-    document.querySelector('#quartoSemFalemais').textContent = formatarMoeda(quarto);  
-        //  Fazendo com o plano faleMais
-        Tempo = 200 - 30;
-        if(Tempo > 0 ){
-           Tempo *= 0;
-           porcentagem = Tempo * 0.10;
-           quarto =  Tempo + porcentagem; 
-           document.querySelector('#quartoComFalemais').textContent = formatarMoeda(quarto);    
-        }else{
-            quarto = 0;
-            document.querySelector('#quartoComFalemais').textContent = formatarMoeda(quarto); 
+            
+             
+             `;
+             
+             document.querySelector('#historico').appendChild(tr);
+     
+     
+          this.reset();
+              
+    }   else if(origem == '016' && destino == '011'){
+        semFale = minutoFixo * 2.90;
+           if(plano == 'FaleMais30'){
+               tempo = 30;
+           } else if(plano == 'FaleMais60'){
+               tempo = 60;
+           } else if(plano == 'FaleMais120'){
+               tempo = 120;
+           }
+           minuto -= tempo;
+           if(minuto > 0){
+               minuto *= 2.90;
+               porcentagem = minuto * 0.10;
+               comFale =  minuto + porcentagem; 
+           } else { 
+           comFale = 0;
+           
         }
-
-    } else{
-        document.querySelector('#quartoSemFalemais').textContent = "-"; 
-        document.querySelector('#quartoComFalemais').textContent = "-"; 
-    }
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${origem}</td>
+        <td>${destino}</td>
+        <td>${plano}</td>
+        <td>${minutoFixo}</td>
+        <td>${formatarMoeda(comFale)}</td>
+        <td>${formatarMoeda(semFale)}</td>
+    
        
-} 
+        
+        `;
+        
+        document.querySelector('#historico').appendChild(tr);
 
+
+     this.reset();
+
+    } else if(origem == '011' && destino == '017'){
+        semFale = minutoFixo * 1.70;
+           if(plano == 'FaleMais30'){
+               tempo = 30;
+           } else if(plano == 'FaleMais60'){
+               tempo = 60;
+           } else if(plano == 'FaleMais120'){
+               tempo = 120;
+           }
+          minuto -= tempo;
+           if(minuto > 0){
+               minuto *= 1.70;
+               porcentagem = minuto * 0.10;
+               comFale =  minuto + porcentagem; 
+           } else  { 
+           comFale = 0;
+        }
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${origem}</td>
+        <td>${destino}</td>
+        <td>${plano}</td>
+        <td>${minutoFixo}</td>
+        <td>${formatarMoeda(comFale)}</td>
+        <td>${formatarMoeda(semFale)}</td>
+    
+       
+        
+        `;
+        
+        document.querySelector('#historico').appendChild(tr);
+
+
+     this.reset();
+
+   } else if(origem == '017' && destino == '011'){
+    semFale = minutoFixo * 2.70;
+       if(plano == 'FaleMais30'){
+           tempo = 30;
+       } else if(plano == 'FaleMais60'){
+           tempo = 60;
+       } else if(plano == 'FaleMais120'){
+           tempo = 120;
+       }
+      minuto -= tempo;
+  
+       if(minuto > 0){
+           minuto *= 2.70;
+           porcentagem = minuto * 0.10;
+           comFale =  minuto + porcentagem; 
+       } else { 
+       comFale = 0;
+    }
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${origem}</td>
+    <td>${destino}</td>
+    <td>${plano}</td>
+    <td>${minutoFixo}</td>
+    <td>${formatarMoeda(comFale)}</td>
+    <td>${formatarMoeda(semFale)}</td>
+    
+   
+    
+    `;
+    
+    document.querySelector('#historico').appendChild(tr);
+
+
+ this.reset();
+
+} else if(origem == '011' && destino == '018'){
+    semFale = minutoFixo * 0.90;
+       if(plano == 'FaleMais30'){
+           tempo = 30;
+       } else if(plano == 'FaleMais60'){
+           tempo = 60;
+       } else if(plano == 'FaleMais120'){
+           tempo = 120;
+       }
+       minuto -= tempo;
+       if(minuto > 0){
+           minuto *= 0.90;
+           porcentagem = minuto * 0.10;
+           comFale =  minuto + porcentagem; 
+       } else { 
+       comFale = 0;
+    }
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${origem}</td>
+    <td>${destino}</td>
+    <td>${plano}</td>
+    <td>${minutoFixo}</td>
+    <td>${formatarMoeda(comFale)}</td>
+    <td>${formatarMoeda(semFale)}</td>
+    
+    
+   
+    
+    `;
+    
+    document.querySelector('#historico').appendChild(tr);
+
+
+ this.reset();
+
+} else if(origem == '018' && destino == '011'){
+    semFale = minutoFixo * 1.90;
+       if(plano == 'FaleMais30'){
+           tempo = 30;
+       } else if(plano == 'FaleMais60'){
+           tempo = 60;
+       } else if(plano == 'FaleMais120'){
+           tempo = 120;
+       }
+       minuto -= tempo;
+       if(minuto > 0){
+           minuto *= 1.90;
+           porcentagem = minuto * 0.10;
+           comFale =  minuto + porcentagem; 
+       } else { 
+       comFale = 0;
+    }
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${origem}</td>
+    <td>${destino}</td>
+    <td>${plano}</td>
+    <td>${minutoFixo}</td>
+    <td>${formatarMoeda(comFale)}</td>
+    <td>${formatarMoeda(semFale)}</td>
+    
+   
+
+    `;
+    
+    document.querySelector('#historico').appendChild(tr);
+
+
+ this.reset();
+
+ } else {
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${origem}</td>
+    <td>${destino}</td>
+    <td>${plano}</td>
+    <td>${minutoFixo}</td>
+    <td>${'-'}</td>
+    <td>${'-'}</td>
+    
+    
+    `;
+    
+    document.querySelector('#historico').appendChild(tr);
+
+
+ this.reset();
+
+ }
+
+
+})
 
   
